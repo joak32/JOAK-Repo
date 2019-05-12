691eed8d6da52c8b5187d274379d1cfafdad9535
joak32 <oakxx011@umn.edu>
2019-05-12 15:02:29 -0500

    Add files via upload
    before clean

--- TodoList/todoList.js
+++ TodoList/todoList.js
@@ -13,24 +13,7 @@ function makeDismiss() {
     return dismissCode;
 }
 
-/*function checkDeadline() {
-    if (newDeadline < 3) {
-        taskList.className += "alert-danger";
-    }
-    else if (newDeadline < 7 && newDeadline > 3) {
-        taskList.className += "alert-warning";
-    }
-    else if (newDeadline >= 7) {
-        taskList.className += "alert-secondary";
-    }
-    // taskList.innerHTML += ""
-    //document.getElementById("submittedTask").innerHTML += "alert-danger";
-
-    
-
-}*/
-
-function addTask() {
+/*function addTask() {
     validateForms();
     var newTask = document.forms["taskForm"]["enterTask"].value;
     var newTask2 = document.getElementById("enterTask");
@@ -46,16 +29,22 @@ function addTask() {
     //taskList.className += "col-sm";
     //taskList.innerHTML += "<div class='col-sm'>"
     if (newDeadline < 3) {
-        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-danger alert-dismissable fade show'>"
-        + newTask + "<br>" + newDeadline + " day(s)" + "</div>";
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-danger alert-dismissable fade show' role='alert'>"
+            + newTask + "<br>" + newDeadline + " day(s)" +
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";
     }
     else if (newDeadline < 7 && newDeadline > 2) {
-        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-warning alert-dismissable fade show'>" +
-            newTask + "<br>" + newDeadline + " day(s)" + "</div>";
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-warning alert-dismissable fade show' role='alert'>" +
+            newTask + "<br>" + newDeadline + " day(s)" + 
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";"</div>";
     }
     else if (newDeadline >= 7) {
-        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-secondary alert-dismissable fade show'>" +
-            newTask + "<br>" + newDeadline + " day(s)" + "</div>";
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-secondary alert-dismissable fade show' role='alert'>" +
+            newTask + "<br>" + newDeadline + " day(s)" + 
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";"</div>";
     }
     //checkDeadline();
 
@@ -65,11 +54,12 @@ function addTask() {
         colCount = 0;
     }
     document.getElementById("display").innerHTML.display = taskList.innerHTML;
-    event.preventDefault();
+    //event.preventDefault();
     taskForm.reset();
+    return false;
     //taskList('.alert').alert();
     //document.getElementById('display').setAttribute('class', "close");
-}
+} WORKING ISH*/
 
 function addDismiss() {
     //add dismiss button
@@ -100,24 +90,24 @@ function clearErrors() {
 
 //check that the days read as a number, non-negative, non-zero
 function validateForms() {
-    //if invalid, throw error
     clearErrors();
-    var inputDays = document.forms["taskForm"]["inputDaysToComplete"].value;
     var inputTaskName = document.forms["taskForm"]["enterTask"].value;
+    var inputDays = document.forms["taskForm"]["inputDaysToComplete"].value;
 
-
-
-    if (inputTaskName === "") {
+    if (inputTaskName == "") {
         alert("Please enter a task");
-        inputTaskName.parentElement.className = "form-group has error";
+        document.forms["taskForm"]["enterTask"].parentElement.className = "form-group has-error";
         document.forms["taskForm"]["enterTask"].focus();
+        //event.preventDefault();
+        return false;
     }
     
-    if (inputDays == "" || isNaN(inputDays)) {
+    if (inputDays == "" || isNaN(inputDays) || inputDays < 1) {
         alert("Please enter a number no less than 1.");
-        document.forms["taskForm"]["inputDays"]
-            .parentElement.className = "form-group has error";
-        document.forms["taskForm"]["inputDays"].focus();
+        document.forms["taskForm"]["inputDaysToComplete"].parentElement.className = "form-group has-error";
+        document.forms["taskForm"]["inputDaysToComplete"].focus();
+        //event.preventDefault();
+        return false;
     }
       /* if (inputTaskName === "") {
         inputTaskName.setCustomValidity("Please fill out this field.");
@@ -125,4 +115,52 @@ function validateForms() {
             .parentElement.className = "form-group has error";
         document.forms["taskForm"]["inputTaskName"].focus();    
     }*/
+    //event.preventDefault();
+
+    var newTask = document.forms["taskForm"]["enterTask"].value;
+    var newTask2 = document.getElementById("enterTask");
+    var newDeadline = document.forms["taskForm"]["inputDaysToComplete"].value;
+    //var fullTask = "";
+    //fullTask.innerHTML = newTask + "<br>" + newDeadline;
+    var taskList = document.getElementById("display");
+    //var taskList = document.createElement("col-sm");
+    //taskList.addClass(fullTask);
+   //document.getElementById("addedTasks").innerText = (newTask + "<br>" + newDeadline + " days");
+    //taskList.innerHTML = "";
+    // doesnt work taskList.append("<div class = 'col-sm'>" + newTask + "<br>" + newDeadline);
+    //taskList.className += "col-sm";
+    //taskList.innerHTML += "<div class='col-sm'>"
+    if (newDeadline < 3) {
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-danger alert-dismissable fade show' role='alert'>"
+            + newTask + "<br>" + newDeadline + " day(s)" +
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";
+    }
+    else if (newDeadline < 7 && newDeadline > 2) {
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-warning alert-dismissable fade show' role='alert'>" +
+            newTask + "<br>" + newDeadline + " day(s)" + 
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";"</div>";
+    }
+    else if (newDeadline >= 7) {
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-secondary alert-dismissable fade show' role='alert'>" +
+            newTask + "<br>" + newDeadline + " day(s)" + 
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" 
+            + "<span aria-hidden='true'>" + "&times;" + "</span>" + "</button></div>";"</div>";
+    }
+    //checkDeadline();
+
+    colCount += 1;
+    if (colCount > 12) {
+        taskList.innerHTML += "</div>" + "<br>" + "<div class='row'>";
+        colCount = 0;
+    }
+    document.getElementById("display").innerHTML.display = taskList.innerHTML;
+    //event.preventDefault();
+    taskForm.reset();
+    return false;
+    //taskList('.alert').alert();
+    //document.getElementById('display').setAttribute('class', "close");
 }
+/*    return false;
+} WORKING ISH*/
