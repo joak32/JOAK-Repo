cf6f609c0d108f17ec0746aa72ff4414b5ac6aec
joak32 <oakxx011@umn.edu>
2019-05-12 13:03:28 -0500

    Add files via upload

--- TodoList/todoList.js
+++ TodoList/todoList.js
@@ -13,6 +13,23 @@ function makeDismiss() {
     return dismissCode;
 }
 
+/*function checkDeadline() {
+    if (newDeadline < 3) {
+        taskList.className += "alert-danger";
+    }
+    else if (newDeadline < 7 && newDeadline > 3) {
+        taskList.className += "alert-warning";
+    }
+    else if (newDeadline >= 7) {
+        taskList.className += "alert-secondary";
+    }
+    // taskList.innerHTML += ""
+    //document.getElementById("submittedTask").innerHTML += "alert-danger";
+
+    
+
+}*/
+
 function addTask() {
     validateForms();
     var newTask = document.forms["taskForm"]["enterTask"].value;
@@ -20,7 +37,7 @@ function addTask() {
     var newDeadline = document.forms["taskForm"]["inputDaysToComplete"].value;
     //var fullTask = "";
     //fullTask.innerHTML = newTask + "<br>" + newDeadline;
-    var taskList = document.getElementById("display")
+    var taskList = document.getElementById("display");
     //var taskList = document.createElement("col-sm");
     //taskList.addClass(fullTask);
    //document.getElementById("addedTasks").innerText = (newTask + "<br>" + newDeadline + " days");
@@ -29,17 +46,19 @@ function addTask() {
     //taskList.className += "col-sm";
     //taskList.innerHTML += "<div class='col-sm'>"
     if (newDeadline < 3) {
-        taskList.innerHTML += "<div class='col-auto mr-auto alert alert-danger alert-dismissable fade show'>"
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-danger alert-dismissable fade show'>"
         + newTask + "<br>" + newDeadline + " day(s)" + "</div>";
     }
     else if (newDeadline < 7 && newDeadline > 2) {
-        taskList.innerHTML += "<div class='col-auto mr-auto alert alert-warning'>" +
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-warning alert-dismissable fade show'>" +
             newTask + "<br>" + newDeadline + " day(s)" + "</div>";
     }
     else if (newDeadline >= 7) {
-        taskList.innerHTML += "<div class='col-auto mr-auto alert alert-secondary'>" +
+        taskList.innerHTML += "<div class='col-auto mr-2 alert alert-secondary alert-dismissable fade show'>" +
             newTask + "<br>" + newDeadline + " day(s)" + "</div>";
     }
+    //checkDeadline();
+
     colCount += 1;
     if (colCount > 12) {
         taskList.innerHTML += "</div>" + "<br>" + "<div class='row'>";
@@ -48,6 +67,22 @@ function addTask() {
     document.getElementById("display").innerHTML.display = taskList.innerHTML;
     event.preventDefault();
     taskForm.reset();
+    //taskList('.alert').alert();
+    //document.getElementById('display').setAttribute('class', "close");
+}
+
+function addDismiss() {
+    //add dismiss button
+    var addButton = document.createElement("button");
+    addButton.innerHTML = "class='close' data-dismiss='alert'";
+    //on dismiss button, add data-dismiss="alert" ATTRIBUTE, triggers function
+    //use <button> 
+    
+    //add .fade and .show classes
+
+    //add .alert-dismissable class (adds padding to right of alert/positions .close button)
+
+    //
 }
 
 //clean this
@@ -57,11 +92,10 @@ function clearErrors() {
         i++) {
             if (document.forms["taskForm"].elements[i]
         .parentElement.className.indexOf("has-") != -1) {
-
             document.forms["taskForm"].elements[i]
             .parentElement.className = "form-group";
         }
-        }
+    }
 }
 
 //check that the days read as a number, non-negative, non-zero
@@ -69,11 +103,26 @@ function validateForms() {
     //if invalid, throw error
     clearErrors();
     var inputDays = document.forms["taskForm"]["inputDaysToComplete"].value;
-    if (inputDays == "" || isNaN(inputDays)) {
-        alert("Please enter a number.");
-        document.forms["taskForm"]["inputDays"]
-        .parentElement.className = "form-group has error";
-        document.forms["taskForm"]["inputDays"].focus();
-        return false;
+    var inputTaskName = document.forms["taskForm"]["enterTask"].value;
+
+
+
+    if (inputTaskName === "") {
+        alert("Please enter a task");
+        inputTaskName.parentElement.className = "form-group has error";
+        document.forms["taskForm"]["enterTask"].focus();
     }
+    
+    if (inputDays == "" || isNaN(inputDays)) {
+        alert("Please enter a number no less than 1.");
+        document.forms["taskForm"]["inputDays"]
+            .parentElement.className = "form-group has error";
+        document.forms["taskForm"]["inputDays"].focus();
+    }
+      /* if (inputTaskName === "") {
+        inputTaskName.setCustomValidity("Please fill out this field.");
+        document.forms["taskForm"]["inputTaskName"]
+            .parentElement.className = "form-group has error";
+        document.forms["taskForm"]["inputTaskName"].focus();    
+    }*/
 }
